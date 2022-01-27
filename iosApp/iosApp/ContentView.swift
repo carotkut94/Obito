3965a1c203d652dadb68bf2638f33a6fe6a74f4a
Deathcode <sidhantrajora@Deathcodes-MacBook-Pro.local>
2022-01-27 23:02:02 +0530

    feat(ui-ios-init): intial ios screen with fab

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,16 +1,45 @@
 import SwiftUI
 import shared
 
-struct ContentView: View {
-	let greet = Greeting().greeting()
-
-	var body: some View {
-		Text(greet)
-	}
+struct Item {
+    let uuid = UUID()
+    let value: String
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+struct ContentView: View {
+    @State private var items = [Item]()
+    
+    var body: some View {
+        ZStack {
+            VStack{
+                Text("Servers")
+                List(self.items, id: \.uuid) { item in
+                    Text(item.value)
+                }
+            }
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.items.append(Item(value: "Item"))
+                    }, label: {
+                        Text("+")
+                        .font(.system(.largeTitle))
+                        .frame(width: 77, height: 70)
+                        .foregroundColor(Color.white)
+                        .padding(.bottom, 7)
+                    })
+                    .background(Color.blue)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+                }
+            }
+        }
+    }
 }
